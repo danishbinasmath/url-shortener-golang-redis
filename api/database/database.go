9f732d3cca1f83202a6d94a42ac74999bd8b364e
Muhammad Danish <danishpk175@gmail.com>
2024-04-06 22:34:06 +0530

    Completed

--- api/database/database.go
+++ api/database/database.go
@@ -1,6 +1,6 @@
 package database
 
-import(
+import (
 	"context"
 	"github.com/go-redis/redis/v8"
 	"os"
@@ -10,9 +10,9 @@ var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("DB_ADDR"),
+		Addr:     os.Getenv("DB_ADDR"),
 		Password: os.Getenv("DB_PASS"),
-		DB: dbNo,
+		DB:       dbNo,
 	})
 	return rdb
 }
